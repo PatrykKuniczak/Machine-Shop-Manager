14653dae46c7c0872454415539fcbbaca7b2ed50
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-01 22:10:07 +0100

    chore: add validators for part

--- backend/src/part/part.entity.ts
+++ backend/src/part/part.entity.ts
@@ -1,11 +1,13 @@
 import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {IsNotEmpty} from "class-validator";
 
 
-@Entity()
+@Entity("part")
 export class Part {
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
+    @IsNotEmpty()
     name: string
 }
