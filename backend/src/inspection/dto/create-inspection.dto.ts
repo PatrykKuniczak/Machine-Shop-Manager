0bc5549df34bcb68aa4a04181cf0fd9b5dd2c517
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-03 19:12:38 +0100

    chore: reformat inspection

--- backend/src/inspection/dto/create-inspection.dto.ts
+++ backend/src/inspection/dto/create-inspection.dto.ts
@@ -1,1 +1,19 @@
-export class CreateInspectionDto {}
+import {MaxDate, MinDate} from "class-validator";
+import {Fault} from "../../fault/fault.entity";
+import {Part} from "../../part/part.entity";
+import {Operation} from "../../operation/operation.entity";
+
+
+export class CreateInspectionDto {
+    @MinDate(new Date())
+    @MaxDate(new Date("2100-01-01"))
+    endDate: Date;
+
+    inspectionState: boolean
+
+    fault: Fault[]
+
+    part: Part[]
+
+    operation: Operation[]
+}
--- backend/src/inspection/inspection.entity.ts
+++ backend/src/inspection/inspection.entity.ts
@@ -1,5 +1,4 @@
-import {Column, CreateDateColumn, Entity, JoinTable, OneToMany, PrimaryGeneratedColumn} from "typeorm";
-import {IsBoolean, MaxDate, MinDate} from "class-validator";
+import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
 import {Fault} from "../fault/fault.entity";
 import {Part} from "../part/part.entity";
 import {Operation} from "../operation/operation.entity";
@@ -13,12 +12,9 @@ export class Inspection {
     @CreateDateColumn()
     startDate: Date;
 
-    @MinDate(new Date())
-    @MaxDate(new Date("2100-01-01"))
-    @Column()
+    @Column({nullable: true})
     endDate: Date;
 
-    @IsBoolean()
     @Column()
     inspectionState: boolean
 
