dc26208880416268eb9641a6e52e40a55df7e21a
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-03 17:51:07 +0100

    chore: reformat machineTechnicalData

--- backend/src/machine-technical-data/dto/create-machine-technical-data.dto.ts
+++ backend/src/machine-technical-data/dto/create-machine-technical-data.dto.ts
@@ -1,1 +1,21 @@
-export class CreateMachineTechnicalDataDto {}
+import {IsNotEmpty, Length, Max, MaxDate, MinDate} from "class-validator";
+import {AxisDimension} from "../../axis-dimension/axis-dimension.entity";
+
+
+export class CreateMachineTechnicalDataDto {
+    @Max(20)
+    axlesNumber: number;
+
+    @Length(3, 50)
+    machineType: string;
+
+    @Length(3, 50)
+    driverType: string;
+
+    @IsNotEmpty()
+    workSpace: AxisDimension[];
+
+    @MinDate(new Date("1900-01-01"))
+    @MaxDate(new Date())
+    productionDate: Date;
+}
