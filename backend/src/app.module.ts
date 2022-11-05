854687c4cbaf823f1d5a1df7572d1ca36dfe08b7
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-05 20:50:06 +0100

    chore: add app.module with modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,14 +5,20 @@ import {AxisDimensionModule} from './axis-dimension/axis-dimension.module';
 import {FaultModule} from './fault/fault.module';
 import {PartModule} from './part/part.module';
 import {MachineTechnicalDataModule} from './machine-technical-data/machine-technical-data.module';
-import { OperationModule } from './operation/operation.module';
-import { InspectionModule } from './inspection/inspection.module';
+import {OperationModule} from './operation/operation.module';
+import {InspectionModule} from './inspection/inspection.module';
+import {MessageModule} from './message/message.module';
+import {PositionModule} from './position/position.module';
+import {PermissionModule} from './permission/permission.module';
+import {UserModule} from './user/user.module';
+import {PersonalDatumModule} from './personal-datum/personal-datum.module';
 import typeOrmConfig from "../database/config/typeorm.config";
 
 
 @Module({
     imports: [ConfigModule.forRoot({isGlobal: true}), TypeOrmModule.forRootAsync(typeOrmConfig),
-        AxisDimensionModule, FaultModule, PartModule, MachineTechnicalDataModule, OperationModule, InspectionModule],
+        AxisDimensionModule, FaultModule, PartModule, MachineTechnicalDataModule, OperationModule,
+        InspectionModule, MessageModule, PositionModule, PermissionModule, UserModule, PersonalDatumModule],
     controllers: [],
     providers: []
 })
