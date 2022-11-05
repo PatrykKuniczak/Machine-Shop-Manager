2935d0e4bb1d37cf0818d1e2ded6b410082c3c04
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-05 20:49:52 +0100

    chore: create position resource

---
+++ backend/src/position/dto/create-position.dto.ts
@@ -0,0 +1,8 @@
+import {Permission} from "../../permission/permission.entity";
+
+
+export class CreatePositionDto {
+    name: string;
+
+    permission: Permission
+}
---
+++ backend/src/position/position.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PositionService } from './position.service';
+import { PositionController } from './position.controller';
+
+
+@Module({
+  controllers: [PositionController],
+  providers: [PositionService]
+})
+
+
+export class PositionModule {}
