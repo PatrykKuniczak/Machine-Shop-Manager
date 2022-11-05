74d6e366f18e5d2eba55de0a98c85abcf7afb45d
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-05 20:31:25 +0100

    chore: create permission resource (Without usable machine relation)

---
+++ backend/src/permission/dto/create-permission.dto.ts
@@ -0,0 +1,15 @@
+export class CreatePermissionDto {
+    id: number;
+
+    viewOrder: boolean
+
+    sendOrder: boolean
+
+    viewDrawing: boolean
+
+    sendDrawing: boolean
+
+    takeResource: boolean
+
+    repairing: boolean
+}
