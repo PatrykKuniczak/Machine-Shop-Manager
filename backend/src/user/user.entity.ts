a267efc24e0d4fa6d6f596e70c1d20491199162a
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-20 12:34:36 +0100

    chore: Create order user -> order relation

--- backend/src/user/user.entity.ts
+++ backend/src/user/user.entity.ts
@@ -1,7 +1,8 @@
-import {Column, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn} from "typeorm";
+import {Column, Entity, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn} from "typeorm";
 import {Position} from "../position/position.entity";
 import {Message} from "../message/message.entity";
 import {PersonalDatum} from "../personal-datum/personal-datum.entity";
+import {Order} from "../order/order.entity";
 
 
 @Entity("User")
@@ -9,15 +10,18 @@ export class User {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @OneToOne(() => Position, {onDelete: "CASCADE", cascade: true})
-    position: Position;
-
     @Column("timestamp with time zone")
     hiredDate: Date;
+
+    @OneToOne(() => Position, {onDelete: "CASCADE", cascade: true})
+    position: Position;
 
     @OneToMany(() => Message, (message) => message.id, {cascade: true})
     message: Message[]
 
     @OneToOne(() => PersonalDatum, {onDelete: "CASCADE", cascade: true})
     personalDatum: PersonalDatum;
+
+    @ManyToOne(() => Order, (order) => order.id)
+    order: Order;
 }
