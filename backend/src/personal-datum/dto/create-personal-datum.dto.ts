0ba50c9632eea9305901d6773f1f0adba8fa3fe2
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-26 20:16:48 +0100

    fix: fix birthDate validators in PersonalDatum

--- backend/src/personal-datum/dto/create-personal-datum.dto.ts
+++ backend/src/personal-datum/dto/create-personal-datum.dto.ts
@@ -1,4 +1,4 @@
-import {MinDate, MinLength} from "class-validator";
+import {MaxDate, MinDate, MinLength} from "class-validator";
 
 
 export class CreatePersonalDatumDto {
@@ -11,6 +11,7 @@ export class CreatePersonalDatumDto {
     @MinLength(3)
     birthCity: string;
 
-    @MinDate(new Date())
+    @MinDate(new Date("1900-01-01"))
+    @MaxDate(new Date())
     birthDate: Date;
 }
