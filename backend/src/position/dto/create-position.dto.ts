438fb91913622524af806a32ab241360d23f64de
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-26 20:19:18 +0100

    chore: Create validator for PositionDto

--- backend/src/position/dto/create-position.dto.ts
+++ backend/src/position/dto/create-position.dto.ts
@@ -1,7 +1,9 @@
 import {Permission} from "../../permission/permission.entity";
+import {MinLength} from "class-validator";
 
 
 export class CreatePositionDto {
+    @MinLength(5)
     name: string;
 
     permission: Permission;
