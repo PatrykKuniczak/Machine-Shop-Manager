580136bc8b1798abf2a5b953b621e1460d2e16fd
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-26 19:40:33 +0100

    style: create space

--- backend/src/instrument/instrument.module.ts
+++ backend/src/instrument/instrument.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { InstrumentController } from './instrument.controller';
 import { InstrumentService } from './instrument.service';
 
+
 @Module({
   controllers: [InstrumentController],
   providers: [InstrumentService]
 })
+
+
 export class InstrumentModule {}
