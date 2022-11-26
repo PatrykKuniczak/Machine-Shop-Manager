9059e221cc1cea86cb920cc97db314aab4873b51
PatrykKuniczak <64608510+PatrykKuniczak@users.noreply.github.com>
2022-11-26 19:38:57 +0100

    Fix: Add name colum for instrument

--- backend/src/instrument/dto/create-instrument.dto.ts
+++ backend/src/instrument/dto/create-instrument.dto.ts
@@ -1,7 +1,10 @@
-import {Min} from "class-validator";
+import {Min, MinLength} from "class-validator";
 
 
 export class CreateInstrumentDto {
+    @MinLength(5)
+    name: string;
+    
     @Min(1)
     length: number;
 
